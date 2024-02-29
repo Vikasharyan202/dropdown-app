7767e9906e0112ace494acba62ac59be14fc53e5
202vikash@gmail.com <vikash_aryan@202vikashgmailcoms-MacBook-Air.local>
2024-02-29 14:18:30 +0530

    starting dropdown app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='dropdown-container'>
+      <h1>Should you use a dropdown?</h1>
+      <div>
+      <button className='btn' onClick={() => setIsOpen((prev) => !prev)}>Dropdown
+      {!isOpen?
+        <img className='icon' src='https://cdn3.iconfinder.com/data/icons/faticons/32/arrow-up-01-512.png'/>:
+        <img className='icon' src='https://www.freeiconspng.com/thumbs/arrow-down-icon-png/black-arrow-down-icon-png-16.png'/>
+      }
+      </button>
+      </div>
     </div>
   );
 }
---
+++ src/mockdata.js
@@ -0,0 +1,9 @@
+const data = [
+  {
+    option:"Yes"
+  },
+  {
+    option: "Probably not"
+  }
+];
+export default data;
